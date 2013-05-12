1ce1b46180cf39056519cf3c2a849619e89858d3
Martin Worm-Leonhard <mwormleonhard@gmail.com>
2013-05-12 21:39:19 +0200

    Initial commit - "Hello World"

---
+++ templates/insertTemplate.js
@@ -0,0 +1,15 @@
+// TeXworksScript
+// Title: insertTemplate
+// Description: Insertion of predefined templates in TeX
+// Author: Martin Worm-Leonhard
+// Version: 0.1
+// Date: 2013-05-12
+// Script-Type: standalone
+// Context: TeXDocument
+// Shortcut: Ctrl+Shift+T
+
+function insertTemplate(){
+	TW.target.insertText("Hej")
+}
+
+insertTemplate()
