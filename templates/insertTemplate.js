563384aa9a7086a049b5b609897d1a5fac2f8946
Martin Worm-Leonhard <mwormleonhard@gmail.com>
2013-05-14 00:33:14 +0200

    Basic functionality implemented. \n Changed template.txt format a bit

--- templates/insertTemplate.js
+++ templates/insertTemplate.js
@@ -10,28 +10,39 @@
 
 function insertTemplate(templateToInsert)
 {
-	TW.target.insertText(templateToInsert);
+	TW.target.insertText(templateToInsert); //Superfluous? Insert directly? 
 }
 
 function readTemplateFile() 
 {
 	var Templates = TW.readFile("templates.txt");
-	return Templates.result.toString();
+	return Templates.result.toString();     //TODO: error handling and format clearing. Is it a string already?
 }
 
 var entireTemplateFile = readTemplateFile();
-var header = /#\s.*\n/g;
+var header = /###(?!END).*###/g; //Matches template header but not end marker 
 var templateHeaders = entireTemplateFile.match(header);
-var templateList = templateHeaders; //.split("\n");
+var templateList = templateHeaders.join(";").replace(/#/g, "").split(";");
 
-insertTemplate(templateHeaders);
-insertTemplate(entireTemplateFile);
+//insertTemplate(templateHeaders);
+//insertTemplate(entireTemplateFile);
 
 var selectedTemplate = TW.getItem(null, 
 "Template / Snippet Selector"
- , "Select template to inesert"
+ , "Select template to insert"
  , templateList
  , 0  // Default selection - zero based index
  , false // true here means can edit
  ); 
- insertTemplate(selectedTemplate);
+ 
+ function extractTemplate(templateName) //TODO: Strip comment lines here or when reading file
+ {
+	 var chunks = entireTemplateFile.split("###END_TEMPLATE###"); //Split into chunks
+	 for (var i = 0; i < chunks.length; i++) 
+	 {
+		 if (chunks[i].match(templateName))  //Find chunk name
+		 return chunks[i].split(/###/)[2];  // Strip header and return chunk body
+	 }
+}
+ 
+ insertTemplate(extractTemplate(selectedTemplate));
