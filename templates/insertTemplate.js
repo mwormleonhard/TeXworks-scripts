fecd1eba5e3c34672602d56653be3da289804526
Martin Worm-Leonhard <mwormleonhard@gmail.com>
2013-05-14 23:16:09 +0200

    Finishing touches
    Added basic file-system-access-error handling.
    Refactored functions and wrapped loose variables.
    Basic functionality done.

--- templates/insertTemplate.js
+++ templates/insertTemplate.js
@@ -1,48 +1,51 @@
 // TeXworksScript
-// Title: insertTemplate
-// Description: Insertion of predefined templates in TeX
+// Title: Insert template
+// Description: Insert a predefined templates in the document
 // Author: Martin Worm-Leonhard
 // Version: 0.1
-// Date: 2013-05-12
+// Date: 2013-05-14
 // Script-Type: standalone
 // Context: TeXDocument
 // Shortcut: Ctrl+Shift+T
 
-function insertTemplate(templateToInsert)
-{
-	TW.target.insertText(templateToInsert); //Superfluous? Insert directly? 
-}
+TW.target.insertText(selectTemplate()); // This is the actual insertion point.
 
 function readTemplateFile() 
 {
-	var Templates = TW.readFile("templates.txt");
-	return Templates.result.toString();     //TODO: error handling and format clearing. Is it a string already?
+    var TemplateFile = TW.readFile("templates.txt");
+    if (TemplateFile.status!=0) 
+    {
+        TW.critical(null, "Cannot read template file!", TemplateFile.message);
+    }
+    else
+    return TemplateFile.result.replace(/^\/\/.*$/gm, "");     //Strip comment lines and return the rest 
 }
 
-var entireTemplateFile = readTemplateFile();
-var header = /###(?!END).*###/g; //Matches template header but not end marker 
-var templateHeaders = entireTemplateFile.match(header);
-var templateList = templateHeaders.join(";").replace(/#/g, "").split(";");
 
-//insertTemplate(templateHeaders);
-//insertTemplate(entireTemplateFile);
-
-var selectedTemplate = TW.getItem(null, 
-"Template / Snippet Selector"
- , "Select template to insert"
- , templateList
- , 0  // Default selection - zero based index
- , false // true here means can edit
- ); 
  
- function extractTemplate(templateName) //TODO: Strip comment lines here or when reading file
+ function selectTemplate() 
  {
-	 var chunks = entireTemplateFile.split("###END_TEMPLATE###"); //Split into chunks
-	 for (var i = 0; i < chunks.length; i++) 
-	 {
-		 if (chunks[i].match(templateName))  //Find chunk name
-		 return chunks[i].split(/###/)[2];  // Strip header and return chunk body
-	 }
-}
+    var entireTemplateFile = readTemplateFile();
+    var header = /###(?!END).*###/g; //Matches template header but not end marker 
+    var templateHeaders = entireTemplateFile.match(header);  //Returns array of headers including ###-tags
+    var templateList = templateHeaders.join(";").replace(/#/g, "").split(";"); //Strips tags and reforms array
+    
+    var selectedTemplate = TW.getItem(null, //Use included UI getItem
+    "Template / Snippet Selector"
+     , "Select template to insert"
+     , templateList // Populates the list
+     , 0  // Default selection - zero based index
+     , false // true here means can edit
+     ); 
+     
+     var chunks = entireTemplateFile.split("###END_TEMPLATE###"); //Split into chunks on end tag. This removes the tag as well.
+     for (var i = 0; i < chunks.length; i++) // Iterate over array
+     {
+         if (chunks[i].match(selectedTemplate)) //Find chunk name
+         {
+            return chunks[i].split(/###/)[2];  // Strip header and return chunk body (index=2)
+         };
+	};
+};
  
- insertTemplate(extractTemplate(selectedTemplate));
+
--- templates/openTemplateFile.js
+++ templates/openTemplateFile.js
@@ -1,9 +1,9 @@
 // TeXworksScript
-// Title: openTemplateFile
-// Description: Edit of predefined templates in TeX
+// Title: Open template file
+// Description: Edit predefined templates for script "Insert template"
 // Author: Martin Worm-Leonhard
 // Version: 0.1
-// Date: 2013-05-12
+// Date: 2013-05-14
 // Script-Type: standalone
 // Context: TeXDocument
 // Shortcut:
@@ -11,9 +11,10 @@
 
 
 var retVal = TW.app.openFileFromScript(TW.script.fileName.replace("openTemplateFile.js","templates.txt"), TW, -1,true); 
-if (retVal.status == 0) // SystemAccess_OK
-{
-	var openedDocument = retVal["result"] ;
-TW.information(null,"Tw Message",openedDocument.fileName); // shows the opened document's base filename
-TW.information(null,"Tw Message",openedDocument.text); // shows the contents of the "opened" document.
-}
+if (retVal.status != 0) // SystemAccess_OK
+	{
+		if (retVal.status==1) msg="System: Access failed! \n Does the file exist?";
+		if (retVal.status==2) msg="System: Access Denied! \n Do you have write permission?";
+		else msg="Unknown error! \n System returned status " + retVal.status;
+		TW.critical(null, "Cannot open template file!", msg);
+	}
